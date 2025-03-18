a74427e5e6f57bbac03117b29699630910aba947
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2025-03-18 17:10:27 +0300

    gitignore

--- config.py
+++ config.py
@@ -6,11 +6,9 @@ load_dotenv()
 
 class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DATABASE_URL")
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "pMRwwlGnPBvFWGHTc-7ulJ9odt4yXd3AysAQbj1AJnY=")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    REDIS_URL: str = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+    REDIS_URL: str = os.getenv("REDIS_URL")
 
 settings = Settings()
-
-DATABASE_URL = f'postgresql+asyncpg://postgres:postgres@127.0.0.1:5432/short_url'
